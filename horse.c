692aea29754dc00b894d9f6ad1ae91e032a60d28
Anapah <70201574+reactxsw@users.noreply.github.com>
2022-02-03 11:20:57 +0700

    Update horse.c

--- horse.c
+++ horse.c
@@ -3,9 +3,21 @@
 #include <string.h>
 #include <stdlib.h>
 
+int i, j, k;
 void delay(int seconds) {
     clock_t start = clock();
     while (clock() < start + 1000 * seconds);
+}
+
+void load(int horse[]) {
+    printf("==============================\n");
+    for (j = 0; j < 5; j++) {
+        printf("Horse %d", j + 1);
+        for (k = 0; k < horse[j]; k++) {
+            printf("-");
+        }
+        printf("X\n");
+    }
 }
 
 int main() {
@@ -17,45 +29,34 @@ int main() {
         rand() % (4) + 3,
         rand() % (4) + 3
     };
-    int userhorse, i, j, k;
+    int userhorse;
     int run = 1;
     printf("Choose a horse (1-5) :");
     scanf("%d", & userhorse);
-    printf("==============================\n");
-    for (j = 0; j < 5; j++) {
-        printf("Horse %d", j + 1);
-        for (k = 0; k < horse[j]; k++) {
-            printf("-");
-        }
-        printf("X\n");
-    }
-    while (run == 1) {
-        delay(1);
-        int numOhorseMove = rand() % 5;
-        for (i = 0; i < numOhorseMove; i++) {
-            int x = rand() % 5;
-            horse[x] = horse[x] - 1;
-            printf("==============================\n");
-            for (j = 0; j < 5; j++) {
-                printf("Horse %d", j + 1);
-                for (k = 0; k < horse[j]; k++) {
-                    printf("-");
+    if (!(userhorse > 5 || userhorse < 1)) {
+        load(horse);
+        while (run == 1) {
+            delay(1);
+            int numOhorseMove = rand() % 5;
+            for (i = 0; i < numOhorseMove; i++) {
+                int x = rand() % 5;
+                horse[x] = horse[x] - 1;
+                load(horse);
+                if (horse[x] == 0) {
+                    run = 0;
+                    printf("==============================\n");
+                    printf("Horse [%d] won\n", x + 1);
+                    if (x + 1 == userhorse) {
+                        printf("You won");
+                    } else {
+                        printf("You lose");
+                    }
+                    break;
                 }
-                printf("X\n");
-            }
-            if (horse[x] == 0) {
-                run = 0;
-                printf("==============================\n");
-                printf("Horse [%d] won\n", x + 1);
-                if (x + 1 == userhorse) {
-                    printf("You won");
-                } else {
-                    printf("You lose");
-                }
-                break;
             }
         }
+    } else {
+        printf("No horse %d",userhorse);
     }
-
     return 0;
 }
